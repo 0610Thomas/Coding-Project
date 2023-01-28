bc875414a10d7e072ea8a94ca811ef1f54861bba
SKim <72927837+0610Thomas@users.noreply.github.com>
2023-01-28 12:19:08 -0500

    Update main.c

--- dot_and_cross_product/main.c
+++ dot_and_cross_product/main.c
@@ -1,34 +1,42 @@
 #include <stdio.h>
 
-float dotProduct(float *firstVector, float *secondVector) {
+float dotProduct(float *firstVector, float flength, float *secondVector, float slength) {
 
         float answer = 0.0;
-        int sf = sizeof(firstVector);
-        int ss = sizeof(secondVector);
+        int sf = flength; // sizeof(firstVector);
+        int ss = slength; //sizeof(secondVector); //Had firstVector instead of secondVector
 
+        //Problem: both sf and ss return the max size of a float array, or 8 as a result of the sizeof method.
+        // Solution: one way is to have the length fixed.
         printf("%i sf %i ss\n", sf, ss);
 
-        if(sf != ss){
+	//checks if the size of both vectors is the same, if not, returns 0
+        if(sf != ss){ 
+	//a zero vector is a legitimate answer that is not the same as No solution
+	// So, print that there is no solution
+                printf(“No solution\n”;)
                 return 0.0;
-        }
 
+        } 
+        //returns dot product
         for(int i = 0; i < 3; i++) {
-                answer += firstVector[i] * secondVector[i];
+        answer += firstVector[i] * secondVector[i];
         }
-
         return answer;
 
 }
 
-float *crossProduct(float *firstVector, float *secondVector) {
+float *crossProduct(float *firstVector, float flength, float *secondVector, float slength) {
 
-        int sf = sizeof(firstVector);
-        int ss = sizeof(secondVector);
+        int sf = flength; // sizeof(firstVector);
+        int ss = slength; //sizeof(secondVector);
 
-        if(sf != ss && sf != 3){
-                return 0;
-        }
         float answer[3];
+        if(sf != ss && sf != 3){
+                printf("No solution\n");
+                float *an = answer;
+                return an;
+        }
         answer[0] = firstVector[2]*secondVector[3] - secondVector[2]*firstVector[3];
         answer[1] = -(firstVector[1]*secondVector[3] - secondVector[1]*firstVector[3]);
         answer[2] = firstVector[1]*secondVector[2] - secondVector[1]*firstVector[2];
@@ -40,13 +48,23 @@ int main() {
 
         float array1[] = {1,2,3};
         float array2[] = {4,5};
-        printf("this is a dot product: %9.5f\n", dotProduct(array1, array2));
+        printf("this is a dot product: %9.5f\n", dotProduct(array1, 3, array2, 2));
 
-        float *c = crossProduct(array1, array2);
+        float *c = crossProduct(array1, 3, array2, 2);
         for(int i = 0; i < 3; i++) {
-                printf("%9.5f\n", c[i]);
+        printf("%9.5f\n", c[i]);
         }
+        
+        printf("Second test:\n Dot product: %9.5f\n", dotProduct(array1, 3, array3, 3));
+
+        float *b = crossProduct(array1, 3, array3, 3);
+        for(int i = 0; i < 3; i++) {
+                printf("%9.5f\n", b[i]);
+        }
+
 
         return 0;
 }
+
+
 
